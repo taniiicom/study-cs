a4398233547ef82eb05670a625df833eb83a9e30
taniiicom <mail@taniii.com>
2023-01-11 11:24:16 +0900

    hotfix: heapsort-php

--- B1/algorithm/heapsort.php
+++ B1/algorithm/heapsort.php
@@ -12,27 +12,30 @@ class Solution {
     // インスタンス変数
     public $nums;
     public $len;
+    public $heapsize;
 
     function sortArray($nums) {
         $this->nums = $nums;
         $this->len = count($this->nums);
+        $this->heapsize = $this->len;
         $this->buildMaxHeapify();
         for($i = $this->len -1; $i > 0; $i--){
             list($this->nums[0], $this->nums[$i]) = array($this->nums[$i], $this->nums[0]);
+            $this->heapsize -= 1;
             $this->maxHeapify(0);
         }
         return $this->nums;
     }
 
     protected function maxHeapify($i) {
-            $il = ($this->len >= $i*2) ? $i*2 : null; // index of light-child
-            $ir = ($this->len >= $i*2+1) ? $i*2+1 : null; // index of light-child
-            if(($il != null)&&($this->nums[$il] > $this->nums[$i])){
+            $il = ($this->len > $i*2+1) ? $i*2+1 : null; // index of light-child
+            $ir = ($this->len > $i*2+2) ? $i*2+2 : null; // index of light-child
+            if(($il != null)&&($il < $this->heapsize)&&($this->nums[$il] > $this->nums[$i])){
                 $imax = $il;  // index of max
             }else{
                 $imax = $i;
             }
-            if(($ir != null)&&($this->nums[$ir] > $this->nums[$il])){
+            if(($ir != null)&&($ir < $this->heapsize)&&($this->nums[$ir] > $this->nums[$imax])){
                 $imax = $ir;
             }
 
